5ccef7512fcf23ae5dd23fad92c0b893d33adc07
Rachem <rachem.goruchon@ingesup.com>
2013-12-18 14:39:41 +0100

    Update 20131218091303_add_column.rb

--- db/migrate/20131218091303_add_column.rb
+++ db/migrate/20131218091303_add_column.rb
@@ -2,5 +2,8 @@ class AddColumn < ActiveRecord::Migration
   def change
       add_column :users, :username, :string
       change_column :users, :email, :string, :default => ""
+      add_column :users, :last_name, :string
+      add_column :users, :provider, :string
+      add_column :users, :uid, :string
   end
 end
