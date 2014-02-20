0ef1c4735f90c375ea917939f484fad6d9882f73
rachem <rachem.goruchon@ingesup.com>
2014-02-20 14:37:22 +0100

    fix issue with openssl

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
   provider :soundcloud, 'dc051eb3245282d8cb8e8b2f991804a2', 'a63f835a010c33774b379b2e716d3d9b'
 end
