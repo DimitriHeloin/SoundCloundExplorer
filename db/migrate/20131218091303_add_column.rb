a90f88f4ad062e79bef4c1ac7a7ff7488270216c
dimimi <dimitri.heloin@ingesup.com>
2014-06-01 12:46:36 +0200

    test

--- db/migrate/20131218091303_add_column.rb
+++ db/migrate/20131218091303_add_column.rb
@@ -4,6 +4,6 @@ class AddColumn < ActiveRecord::Migration
       change_column :users, :email, :string, :default => ""
       add_column :users, :last_name, :string
       add_column :users, :provider, :string
-      add_column :users, :uid, :string
+      add_column :users, :uid, :integer
   end
 end
