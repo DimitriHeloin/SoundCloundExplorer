4ff765ac6025bcc33b8b8cbc022f33474fc01f93
dimimi <dimitri.heloin@ingesup.com>
2014-06-01 11:29:12 +0200

    test

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-  provider :soundcloud, '21b06091d1127ab61686016e2a9d6543', '637db277478292e22d25ebabb16286fe'
+  provider :soundcloud, 'c64e305d7cb97058d42256ffc2528d93', 'e0900245108f1c143ae7524638d5d49d'
 end
