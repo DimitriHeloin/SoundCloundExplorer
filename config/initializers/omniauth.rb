71b159021b669d4c0f9f3f8d258dcd16946b9730
dimimi <dimitri.heloin@ingesup.com>
2014-06-01 11:06:47 +0200

    serve static assets

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-  provider :soundcloud, 'dc051eb3245282d8cb8e8b2f991804a2', 'a63f835a010c33774b379b2e716d3d9b'
+  provider :soundcloud, '21b06091d1127ab61686016e2a9d6543', '637db277478292e22d25ebabb16286fe'
 end
